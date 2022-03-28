325e25c7c145d96b1116e45c693d26a51d29447a
KuyHo <kchung1995@gmail.com>
2022-03-28 21:32:24 +0900

    Initial commit

---
+++ src/test/java/com/tistory/katfun/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tistory.katfun.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
