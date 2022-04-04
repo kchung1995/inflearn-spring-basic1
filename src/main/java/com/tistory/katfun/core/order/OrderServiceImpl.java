68fc98af0cfda5e63ee875c3e230c0001c92fdbb
KuyHo <kchung1995@gmail.com>
2022-04-04 22:47:38 +0900

    새로운 할인 정책 적용과 문제점

--- src/main/java/com/tistory/katfun/core/order/OrderServiceImpl.java
+++ src/main/java/com/tistory/katfun/core/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package com.tistory.katfun.core.order;
 
 import com.tistory.katfun.core.discount.DiscountPolicy;
 import com.tistory.katfun.core.discount.FixedDiscountPolicy;
+import com.tistory.katfun.core.discount.RateDiscountPolicy;
 import com.tistory.katfun.core.member.Member;
 import com.tistory.katfun.core.member.MemberRepository;
 import com.tistory.katfun.core.member.MemoryMemberRepository;
@@ -9,7 +10,9 @@ import com.tistory.katfun.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixedDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new FixedDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    private DiscountPolicy discountPolicy;
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
