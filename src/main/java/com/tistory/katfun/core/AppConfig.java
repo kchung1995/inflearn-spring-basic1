dfa8abdd4774b029b8bab6d35b4a3fea61972915
KuyHo <kchung1995@gmail.com>
2022-04-09 19:01:55 +0900

    AppConfig 리팩토링

--- src/main/java/com/tistory/katfun/core/AppConfig.java
+++ src/main/java/com/tistory/katfun/core/AppConfig.java
@@ -1,6 +1,8 @@
 package com.tistory.katfun.core;
 
+import com.tistory.katfun.core.discount.DiscountPolicy;
 import com.tistory.katfun.core.discount.FixedDiscountPolicy;
+import com.tistory.katfun.core.member.MemberRepository;
 import com.tistory.katfun.core.member.MemberService;
 import com.tistory.katfun.core.member.MemberServiceImpl;
 import com.tistory.katfun.core.member.MemoryMemberRepository;
@@ -10,10 +12,18 @@ import com.tistory.katfun.core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixedDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixedDiscountPolicy();
     }
 }
