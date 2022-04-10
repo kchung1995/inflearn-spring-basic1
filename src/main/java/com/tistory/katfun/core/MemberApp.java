5d159ff21005d69c651195c61f0c8150d1d94a85
KuyHo <kchung1995@gmail.com>
2022-04-10 20:33:16 +0900

    스프링으로 전환하기

--- src/main/java/com/tistory/katfun/core/MemberApp.java
+++ src/main/java/com/tistory/katfun/core/MemberApp.java
@@ -4,12 +4,18 @@ import com.tistory.katfun.core.member.Grade;
 import com.tistory.katfun.core.member.Member;
 import com.tistory.katfun.core.member.MemberService;
 import com.tistory.katfun.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
