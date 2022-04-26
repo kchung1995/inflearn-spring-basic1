c65c129a8ba1d3ad1d7b00225145fd8c1ebbc2e5
KuyHo <kchung1995@gmail.com>
2022-04-26 23:55:12 +0900

    싱글톤 컨테이너

--- src/test/java/com/tistory/katfun/core/singleton/SingletonTest.java
+++ src/test/java/com/tistory/katfun/core/singleton/SingletonTest.java
@@ -4,6 +4,8 @@ import com.tistory.katfun.core.AppConfig;
 import com.tistory.katfun.core.member.MemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -39,5 +41,21 @@ public class SingletonTest {
         assertThat(singletonService1).isSameAs(singletonService2);
         // same ==
         // equal : Java equals
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        // 1. 조회: 호출할 때마다 객체를 생성
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        // 2. 조회: 호출할 때마다 객체를 생성
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        // memberService1 != memberService2
+        assertThat(memberService1).isSameAs(memberService2);
     }
 }
