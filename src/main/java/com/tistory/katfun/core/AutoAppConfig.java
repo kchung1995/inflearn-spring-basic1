a1bd1c3a1c701acd8b653652019d2872de042b5f
KuyHo <kchung1995@gmail.com>
2022-05-18 22:13:41 +0900

    탐색 위치와 기본 스캔 대상

--- src/main/java/com/tistory/katfun/core/AutoAppConfig.java
+++ src/main/java/com/tistory/katfun/core/AutoAppConfig.java
@@ -6,6 +6,9 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        basePackages = "com.tistory.katfun.core.member",
+        basePackageClasses = AutoAppConfig.class,
+        // 지정하지 않으면 default에 따라 componentscan이 속한 패키지부터 뒤진다.
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 // @Component가 붙은 모든 클라스를 찾아서 bean으로 등록해 준다.
