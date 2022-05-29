9dbc3324eb12e9f96b727f652a5af8511a16685b
KuyHo <kchung1995@gmail.com>
2022-05-29 19:58:38 +0900

    중복 등록과 충돌

--- src/main/java/com/tistory/katfun/core/AutoAppConfig.java
+++ src/main/java/com/tistory/katfun/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package com.tistory.katfun.core;
 
+import com.tistory.katfun.core.member.MemberRepository;
+import com.tistory.katfun.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -15,4 +18,11 @@ import org.springframework.context.annotation.FilterType;
 // excludeFilters - 기존 AppConfig 관련 내용이 등록되지 않도록 해제
 public class AutoAppConfig {
 
+    // 수동 등록 빈이 자동 등록 빈에 비해 우선권을 가진다.
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+    // 최근 스프링 부트에서는 수동 빈 등록과 자동 빈 등록이 충돌나면 오류가 발생하도록 기본 값을 바꾸었다.
+    // 스프링 부트인 'CoreApplication' 을 실행하면 알 수 있다.
 }
