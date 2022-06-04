ec076fdfbb79226b3a821fe070b74da90e033abc
KuyHo <kchung1995@gmail.com>
2022-06-04 23:16:50 +0900

    조회 빈이 2개 이상 - 문제

--- src/main/java/com/tistory/katfun/core/discount/RateDiscountPolicy.java
+++ src/main/java/com/tistory/katfun/core/discount/RateDiscountPolicy.java
@@ -2,7 +2,9 @@ package com.tistory.katfun.core.discount;
 
 import com.tistory.katfun.core.member.Grade;
 import com.tistory.katfun.core.member.Member;
+import org.springframework.stereotype.Component;
 
+@Component
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private int discountPercent = 10;
