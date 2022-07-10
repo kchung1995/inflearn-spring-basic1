d3f18233c4f5de7761f1a8f053189eb753cf1a03
KuyHo <kchung1995@gmail.com>
2022-07-11 00:13:51 +0900

    request 스코프 예제 만들기

---
+++ src/main/java/com/tistory/katfun/core/web/LogDemoController.java
@@ -0,0 +1,28 @@
+package com.tistory.katfun.core.web;
+
+import com.tistory.katfun.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    private final MyLogger myLogger;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) {
+        String requestURL = request.getRequestURI().toString();
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("Controller test");
+        logDemoService.logic("testId");
+        return "OK";
+    }
+}
